65309c5e2b8eb8c8ba1113bf4b27cca15ecab550
Justin <justinkelly1995@hotmail.com>
2023-01-13 17:15:59 -0700

    moved kanji display functionality to dictionarydisplay script

--- KanjiInfo.gd
+++ KanjiInfo.gd
@@ -30,27 +30,17 @@ func _ready():
 	List.connect("list_created", self, "_reload_directories")
 	pass # Replace with function body.
 
-#load the currently selected list. If non selected, defaults to full dictionary
-#takes a list nam as argument
+#load list updates current list to selected list. If nothing is passed in, defaults to dictionary
+#additionally, when load list is called, setes page number to 0 (and updates text)
+#updates current_list_name if current_list is not null
+#finally emits a signal to update displayed kanji.
 func load_list(list = null):
 	current_page = 0 #set page to 0 when loading new list.
 	current_list = List.get_list_values(list)
+	page.text = str(current_page + 1) + "/" + str(current_list.size())
 	if List.is_empty(list): current_list_name = null
 	else: current_list_name = list
-	
-	load_entry(current_page)
-
-#pull kanji from current_list[curreent_kanji] 
-func load_entry(entry):
-	current_kanji = JapaneseDictionary.get_kanji(current_list[current_page])
-	
-	#kanji_label.text = current_kanji.get_kanji()
-	#on_yomi.text = current_kanji.get_on_yomi(true, ", ")
-	#kun_yomi.text = current_kanji.get_kun_yomi(true, ", ")
-	#meaning.text = current_kanji.get_meaning(true, ", ")
-	page.text = str(entry + 1) + "/" + str(current_list.size())
-
-
+	emit_signal("change_kanji", current_list[current_page])
 
 #*********************************#
 #SIGNAL functions
@@ -59,13 +49,11 @@ func load_entry(entry):
 #adds currently displayed kanji to list.
 func add_to_list(list):
 	List.add_kanji_to_list(list, current_list[current_page])
-	pass
 
 func _reload_directories():
 	print("reload!")
 	list_selector.reload_directory()
 	list_adder.reload_directory()
-	pass
 
 #navigate to previous kanji page
 func _on_LeftButton_pressed():
@@ -73,8 +61,7 @@ func _on_LeftButton_pressed():
 	if current_page < 0:
 		current_page = current_list.size() - 1		
 	page.text = str(current_page + 1) + "/" + str(current_list.size())
-	emit_signal("change_kanji", current_page)
-	pass # Replace with function body.
+	emit_signal("change_kanji", current_list[current_page])
 
 #navigate to next kanji page
 func _on_RightButton_pressed():
@@ -82,7 +69,7 @@ func _on_RightButton_pressed():
 	if current_page == current_list.size():
 		current_page = 0
 	page.text = str(current_page + 1) + "/" + str(current_list.size())
-	emit_signal("change_kanji", current_page)
+	emit_signal("change_kanji", current_list[current_page])
 
 
 func _on_BackButton_pressed():
